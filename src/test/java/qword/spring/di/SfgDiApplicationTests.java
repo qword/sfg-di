d9097a82340145f44eaaf3681784e47d61b1d776
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-03-30 12:34:00 +0200

    Create Spring project

---
+++ src/test/java/qword/spring/di/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package qword.spring.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
