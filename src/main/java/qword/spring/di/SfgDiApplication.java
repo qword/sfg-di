4e89ef26060471e35af9a60994994c81acc86527
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-03-30 13:06:53 +0200

    Get bean from application context

--- src/main/java/qword/spring/di/SfgDiApplication.java
+++ src/main/java/qword/spring/di/SfgDiApplication.java
@@ -2,12 +2,18 @@ package qword.spring.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import qword.spring.di.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        final MyController controller = (MyController)ctx.getBean("myController");
+        final String greeting = controller.sayHello();
+        System.out.println(greeting);
     }
 
 }
